a7eee7201b0808e4f503f7d176d6a27844de075f
CodePoet80 <jonathan.m.wise@gmail.com>
2018-11-26 22:07:15 -0500

    fix bug in installer

--- Install-OSX.sh
+++ Install-OSX.sh
@@ -32,6 +32,6 @@ echo
 echo To create a VSCode-ready webOS app use:
 echo palm-generate -t new_app \~/Projects/YourNewApp
 echo
-if ["$1" -ne "silent"]; then
-    read -rsp $'Press any key to continue . . .\n' -n1 key
+if [ "$1" != "silent" ]; then
+     read -rsp $'Press any key to continue . . .\n' -n1 key
 fi
